adcbe25027e623b057398b9897b42c255b9ee6de
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-07-24 17:06:02 -0300

    Update Nemesis.py

--- Nemesis.py
+++ Nemesis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import Cache as ch 
+import numpy as np 
 
 def print_cont_arquivo(lista):
 	"""Funcao que serve para verificar se o arquivo foi lido corretamente"""
@@ -22,7 +23,7 @@ def main():
 	print(" ")
 	print("Ele tambem prove a quantidade de acessos realizados, a quantidade de hits e de misses (divididos em misses compulsorios e misses por conflito).")
 	print(" ")
-	print("Lembre-se que o endereco de acesso para uma Cache na arquitetura RISC-V apresenta um tamanho de 64 bits.")
+	print("Trabalharemos com um endereco de tamanho de 32 bits. Logo, o valor decimal inserido deve ser algo entre 0 ate 2e31 - 1. Com ambas as extremidades inclusas.")
 	print(" ")
 	print("Importante lembrar tambem que um bloco presente em uma cache se apresenta dividido em palavras/words. Cada palavra por sua vez apresenta 32 bits, podendo, portanto")
 	print("representar valores numa faixa de -2e31 ate 2e31 - 1.")
@@ -79,6 +80,8 @@ def main():
 	#VAMOS TESTAR AGORA A SELECAO DO MODO DA CACHE
 	print("Selecione qual modo de Cache voce deseja simular: Por exemplo, se vc deseja simular uma Cache Direct-Mapping que apresenta 1 words/block, insira A1. Para mais exemplos, consulte a tabela acima.")
 	modo_cache = input()
+
+
 	''' '''
 	'''VARIAVEIS QUE SERAO UTILIZADAS PARA COMPUTAR O MISS RATE, HIT RATE, ETC ETC'''
 	QTD_ACESSOS = qtd_enderecos
@@ -87,26 +90,113 @@ def main():
 	QTD_MISSES_COMPULSORIOS = 0
 	QTD_MISSES_CONFLITO = 0
 	''' '''
+
+	#CACHE DO TIPO A1
+
 	if modo_cache == "A1": #CACHE DIRECT-MAPPING WITH 1 WORDS/BLOCK
 		#PRIMEIRO PASSO: INICIALIZAR A CACHE
 		memory = ch.Cache("A1")
 		memory.print_cache()
 		#SEGUNDO PASSO, HORA DE CALCULAR OS HITS E OS MISSES....
 		for num_address, data, mode in lista_de_enderecos:
-			#CALCULA O ENDERECO PRIMEIRO. NO CASO TEM QUE CALCULAR O INDEX: 
+			#CALCULA O ENDERECO PRIMEIRO. NO CASO TEM QUE CALCULAR O INDEX:
+			binary_address = np.binary_repr(num_address, 32)
+			sentence = f"Binary Address: {binary_address} ---- Decimal Address: {num_address}"
+			#MASSA, JA PASSEI O ENDERECO PARA BINARIO. HORA DE INSERIR NA CACHE...
+			if mode == 'w':
+				#MODO ESCRITA: SO TEM QUE JOGAR NA CACHE
+				memory.write_cache(num_address, data, binary_address, "A1")
+			elif mode == 'w':
+				hit_miss = memory.read_cache(num_address, data, binary_address, "A1")
+				if hit_miss == 1: #TIVEMOS UM HIT
+					QTD_HITS += 1
+				elif hit_miss == 2: #TIVEMOS UM MISS COMPULSORIO
+					QTD_MISSES += 1
+					QTD_MISSES_COMPULSORIOS += 1
+				elif hit_miss == 3: #TIVEMOS UM MISS POR CONFLITO
+					QTD_MISSES += 1
+					QTD_MISSES_CONFLITO += 1
+
+	#CACHE DO TIPO A2
 
 	elif modo_cache == "A2":#CACHE DIRECT-MAPPING WITH 2 WORDS/BLOCK
 		#PRIMEIRO PASSO: INICIALIZAR A CACHE
 		memory = ch.Cache("A2")
 		memory.print_cache()
+		#SEGUNDO PASSO, HORA DE CALCULAR OS HITS E OS MISSES....
+		for num_address, data, mode in lista_de_enderecos:
+			#CALCULA O ENDERECO PRIMEIRO. NO CASO TEM QUE CALCULAR O INDEX:
+			binary_address = np.binary_repr(num_address, 32)
+			sentence = f"Binary Address: {binary_address} ---- Decimal Address: {num_address}"
+			#MASSA, JA PASSEI O ENDERECO PARA BINARIO. HORA DE INSERIR NA CACHE...
+			if mode == 'w':
+				#MODO ESCRITA: SO TEM QUE JOGAR NA CACHE
+				memory.write_cache(num_address, data, binary_address, "A2")
+			elif mode == 'w':
+				hit_miss = memory.read_cache(num_address, data, binary_address, "A2")
+				if hit_miss == 1: #TIVEMOS UM HIT
+					QTD_HITS += 1
+				elif hit_miss == 2: #TIVEMOS UM MISS COMPULSORIO
+					QTD_MISSES += 1
+					QTD_MISSES_COMPULSORIOS += 1
+				elif hit_miss == 3: #TIVEMOS UM MISS POR CONFLITO
+					QTD_MISSES += 1
+					QTD_MISSES_CONFLITO += 1
+
+	#CACHE DO TIPO A3
+
 	elif modo_cache == "A3":#CACHE DIRECT-MAPPING WITH 4 WORDS/BLOCK
 		#PRIMEIRO PASSO: INICIALIZAR A CACHE
 		memory = ch.Cache("A3")
 		memory.print_cache()
+		#SEGUNDO PASSO, HORA DE CALCULAR OS HITS E OS MISSES....
+		for num_address, data, mode in lista_de_enderecos:
+			#CALCULA O ENDERECO PRIMEIRO. NO CASO TEM QUE CALCULAR O INDEX:
+			binary_address = np.binary_repr(num_address, 32)
+			sentence = f"Binary Address: {binary_address} ---- Decimal Address: {num_address}"
+			#MASSA, JA PASSEI O ENDERECO PARA BINARIO. HORA DE INSERIR NA CACHE...
+			if mode == 'w':
+				#MODO ESCRITA: SO TEM QUE JOGAR NA CACHE
+				memory.write_cache(num_address, data, binary_address, "A3")
+			elif mode == 'w':
+				hit_miss = memory.read_cache(num_address, data, binary_address, "A3")
+				if hit_miss == 1: #TIVEMOS UM HIT
+					QTD_HITS += 1
+				elif hit_miss == 2: #TIVEMOS UM MISS COMPULSORIO
+					QTD_MISSES += 1
+					QTD_MISSES_COMPULSORIOS += 1
+				elif hit_miss == 3: #TIVEMOS UM MISS POR CONFLITO
+					QTD_MISSES += 1
+					QTD_MISSES_CONFLITO += 1
+
+	#CACHE DO TIPO A4
+
 	elif modo_cache == "A4":#CACHE DIRECT-MAPPING WITH 8 WORDS/BLOCK
 		#PRIMEIRO PASSO: INICIALIZAR A CACHE
 		memory = ch.Cache("A4")
 		memory.print_cache()
+		#SEGUNDO PASSO, HORA DE CALCULAR OS HITS E OS MISSES....
+		for num_address, data, mode in lista_de_enderecos:
+			#CALCULA O ENDERECO PRIMEIRO. NO CASO TEM QUE CALCULAR O INDEX:
+			binary_address = np.binary_repr(num_address, 32)
+			sentence = f"Binary Address: {binary_address} ---- Decimal Address: {num_address}"
+			#MASSA, JA PASSEI O ENDERECO PARA BINARIO. HORA DE INSERIR NA CACHE...
+			if mode == 'w':
+				#MODO ESCRITA: SO TEM QUE JOGAR NA CACHE
+				memory.write_cache(num_address, data, binary_address, "A4")
+			elif mode == 'w':
+				hit_miss = memory.read_cache(num_address, data, binary_address, "A4")
+				if hit_miss == 1: #TIVEMOS UM HIT
+					QTD_HITS += 1
+				elif hit_miss == 2: #TIVEMOS UM MISS COMPULSORIO
+					QTD_MISSES += 1
+					QTD_MISSES_COMPULSORIOS += 1
+				elif hit_miss == 3: #TIVEMOS UM MISS POR CONFLITO
+					QTD_MISSES += 1
+					QTD_MISSES_CONFLITO += 1
+
+	#CACHE DO TIPO B1
+
 	'''elif modo_cache == "B1":#CACHE TWO-WAY SET ASSOCIATIVE WITH 1 WORDS/BLOCK
 		pass
 	elif modo_cache == "B2":#CACHE TWO-WAY SET ASSOCIATIVE WITH 2 WORDS/BLOCK
